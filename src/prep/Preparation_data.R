2dff7cb64e0aa64ca4f2981a4bb4344446c249d9
balboa57 <n.bouwhuis@tilburguniversity.edu>
2024-10-02 16:10:39 +0200

    created final preparation document

---
+++ src/prep/Preparation_data.R
@@ -0,0 +1,19 @@
+# Input = filtered_data
+
+# Removing unneccesary columns
+
+# Identify missing values
+
+# Handle missing value
+
+# Identify incorrectly formatted values
+
+# Handle incorrectly formatted values
+
+# Check for duplicates
+
+# Handle duplicates
+
+# Create neccesary variables
+
+# Output = cleaned_data
