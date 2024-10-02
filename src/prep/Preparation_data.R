286322779b44fda11ef8714f59b7e1084a57228f
N. Bouwhuis <163580747+Balboa57@users.noreply.github.com>
2024-10-02 17:08:10 +0200

    Update Preparation_data.R

--- src/prep/Preparation_data.R
+++ src/prep/Preparation_data.R
@@ -1,8 +1,17 @@
 # Input = filtered_data
 
-# Removing unneccesary columns
 
-# Identify missing values
+cleaned_data <- filtered_data %>%
+  # Removing unneccesary columns: primaryTitle/originalTitle, isAdult
+  select(-primaryTitle, -isAdult) %>%
+  # Identify missing values:
+  # Step 1: Replace "\N" with NA
+  mutate(across(everything(), ~ na_if(., "\\N")))
+# Step 2: Count NA values in each column
+na_counts <- filtered_data %>%
+  summarize_all(~ sum(is.na(.)))
+# View the NA counts for each column
+na_counts
 
 # Handle missing value
 
