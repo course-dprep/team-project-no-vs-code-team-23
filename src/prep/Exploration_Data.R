82d0a5a18e52011ca15a7bf459588aed2199f9e7
M.D.Yolovska <m.d.yolovska@tilburguniversity.edu>
2024-10-13 12:15:24 +0200

    Update Exploration_Data.R

--- src/prep/Exploration_Data.R
+++ src/prep/Exploration_Data.R
@@ -1,6 +1,3 @@
-# # Set CRAN mirror at the beginning of the script
-options(repos = c(CRAN = "https://cloud.r-project.org/"))
-
 # Data Exploration
 # ---- Getting Started ----
 install.packages("dplyr")
